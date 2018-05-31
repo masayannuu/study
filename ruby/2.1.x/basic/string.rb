0a4c9e2b477636630d686f9099a9b72415a28474
masayannuu <masayannuu@gmail.com>
2018-05-31 23:48:01 +0900

    add sprintf

--- ruby/2.1.x/basic/string.rb
+++ ruby/2.1.x/basic/string.rb
@@ -1,4 +1,4 @@
-# String literal
+# String
 r = "ruby"
 v = r
 r += "2.1.10"
@@ -6,8 +6,12 @@ p r # => "ruby2.1.10"
 p v # => "ruby"
 # string.<< is destructive method
 
+p v * 3 # => "rubyrubyruby"
+
+p "a" < "b" # => true
+
 a = "variable"
-# % literal
+# %
 # dubble quart
 p %?#{a}hoge? # => "variablehoge"
 p %Q?hoge#{a}? # => "hogevariable"
@@ -22,3 +26,8 @@ p %s*symbol* # => :symbol
 # array
 p %W(i am a men #{a}) # => ["i", "am", "a", "men", "variable"]
 p %w&i am a men& # => ["i", "am", "a", "men"]
+
+#sprintf = string %
+#formatted character
+p sprintf("result:%#b", 16) # => "result:0b10000"
+p "result:%#b" % 16 # => "result:0b10000"
