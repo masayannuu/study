fef94c4f6f47eb85a8b0ef8747ae0a73a12fd844
masayannuu <masayannuu@gmail.com>
2018-06-02 17:03:37 +0900

    add parsent

--- ruby/2.1.x/basic/string.rb
+++ ruby/2.1.x/basic/string.rb
@@ -9,25 +9,3 @@ p v # => "ruby"
 p v * 3 # => "rubyrubyruby"
 
 p "a" < "b" # => true
-
-a = "variable"
-# %
-# dubble quart
-p %?#{a}hoge? # => "variablehoge"
-p %Q?hoge#{a}? # => "hogevariable"
-
-# single quart
-p %q<#{a}hoge> # => "\#{a}hoge"
-p %q[hoge#{a}] # => "hoge\#{a}"
-
-# symbol
-p %s*symbol* # => :symbol
-
-# array
-p %W(i am a men #{a}) # => ["i", "am", "a", "men", "variable"]
-p %w&i am a men& # => ["i", "am", "a", "men"]
-
-#sprintf = string %
-#formatted character
-p sprintf("result:%#b", 16) # => "result:0b10000"
-p "result:%#b" % 16 # => "result:0b10000"
