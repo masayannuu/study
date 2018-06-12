33bb7cf56a3dd5aa3bf038ea8e7c47bc4e701562
masayannuu <masayannuu@gmail.com>
2018-06-12 23:57:23 +0900

    add numeric

---
+++ ruby/2.1.x/basic/numeric.rb
@@ -0,0 +1,12 @@
+p 2.2.ceil # => 3
+p 1.9.floor # => 1
+
+p 2.2.round # => 2
+p 1.9.round # => 2
+
+p 8.9.truncate # => 8
+
+p -1.1.ceil # => -1
+p -1.1.floor # => -2
+p -1.1.round # => -1
+p -1.1.truncate # => -1
