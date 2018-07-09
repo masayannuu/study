6c48a5dea291adad2cfd533d4945e188246c9c6c
masayannuu <masayannuu@gmail.com>
2018-07-10 00:47:21 +0900

    add class nesting

--- ruby/2.1.x/basic/class.rb
+++ ruby/2.1.x/basic/class.rb
@@ -11,21 +11,27 @@ C.class_eval %Q{
 
 p C.new.m # => [:m, 1]
 
-C.class_eval do
-  CONST = "eval_const".freeze
-  def hello
-    CONST
+module M
+  C.class_eval do
+    p Module.nesting # => [M]
+    CONST = "eval_const".freeze
+    def hello
+      CONST
+    end
   end
 end
 
 p C.new.hello # => "eval_const"
 p C.instance_methods(false) # => [:m, :hello]
 
-C.class_eval(<<-CODE)
-  def world
-    CONST
-  end
-CODE
+module M
+  C.class_eval(<<-CODE)
+    p Module.nesting # => [C, M]
+    def world
+      CONST
+    end
+  CODE
+end
 
 p C.new.world # => "hello world"
 p C.new.hello # => "eval_const"
