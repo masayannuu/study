659492b0084f981e990a7c81238f3c2007a21c4e
masayannuu <masayannuu@gmail.com>
2018-07-10 01:02:16 +0900

    add override class defined

--- ruby/2.1.x/basic/class.rb
+++ ruby/2.1.x/basic/class.rb
@@ -36,3 +36,30 @@ end
 p C.new.world # => "hello world"
 p C.new.hello # => "eval_const"
 p C.instance_methods(false) # => [:m, :hello, :world]
+
+
+# override initialize
+class S
+  @@val = 0
+
+  def initialize
+    @@val += 1
+  end
+end
+
+class T < S
+  class << T
+    # call only once when defined.
+    @@val += 1
+  end
+
+  def initialize
+  end
+end
+
+T.new
+T.new
+S.new
+S.new
+
+p T.class_variable_get(:@@val) # => 3
