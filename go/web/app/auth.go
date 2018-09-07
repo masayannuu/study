23fe2d7c4421b8fb4a47d685e1940a5c4feee7bb
masayannuu <masayannuu@gmail.com>
2018-09-08 00:26:55 +0900

    add auth & avatar

---
+++ go/web/app/auth.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	gomniauthcommon "github.com/stretchr/gomniauth/common"
+)
+
+type ChatUser interface {
+	UniqueID() string
+	AvatarURL() string
+}
+
+type chatUser struct {
+	gomniauthcommon.User
+	uniqueID string
+}
+
+func (u chatUser) UniqueID() string {
+	return u.uniqueID
+}
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie || cookie.Value == "" {
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := string.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("認証プロバイダの取得に失敗しました", provider, "-", err)
+		}
+		loginURL, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			log.Fatalln("GetBeginAuthURLの呼び出し中にエラーが発生しました:", provider, "-", err)
+		}
+		//next
+	}
+}
